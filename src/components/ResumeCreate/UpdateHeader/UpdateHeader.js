7c8b088a7aefbbe4370d76c82e00e437657b89ca
Hemanth R <hemanthrt18@gmail.com>
2021-05-31 23:44:26 +0530

    📜

--- src/components/ResumeCreate/UpdateHeader/UpdateHeader.js
+++ src/components/ResumeCreate/UpdateHeader/UpdateHeader.js
@@ -1,7 +1,10 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { useForm } from "react-hook-form";
+import {useDropzone} from 'react-dropzone';
 
+import './UpdateHeader.css'
 import { DetailsContext } from '../../../contexts/DetailsContext'
+import { Button } from '@material-ui/core';
 
 function UpdateHeader() {
 
@@ -10,13 +13,47 @@ function UpdateHeader() {
 
     const onSubmit = (data) => {
         updateHeader(data)
+        console.log(resume.header)
     };
+
 
     return (
         <div className="updateHeader">
             <h1>About You</h1>
             <div className="headerFormContainer">
-                
+                <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="headerFormOne">
+                        <img src="" alt="" />
+                        <div className="headerFormOneRight">
+                            <input 
+                                type="text" 
+                                placeholder="Your Name" 
+                                {...register("name")} 
+                                name="name"
+                                onChange={handleSubmit(onSubmit)}
+                            />
+                            <input 
+                                type="text" 
+                                placeholder="Title" 
+                                {...register("title")} 
+                                name="title"
+                                onChange={handleSubmit(onSubmit)}
+                            />
+                        </div>
+                    </div>
+                    <div className="headerFormTwo">
+                        <textarea 
+                            type="text" 
+                            placeholder="Bio" 
+                            {...register("bio")} 
+                            name="bio"
+                            style={{maxHeight: '5rem'}}
+                            onChange={handleSubmit(onSubmit)}
+                        />
+                    </div>
+
+                    <Button type="submit">Save</Button>
+                </form>
             </div>
         </div>
     )
