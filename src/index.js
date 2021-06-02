6a06c8abc67b797ca3e1bec2673acb144a824cfb
Hemanth R <hemanthrt18@gmail.com>
2021-06-02 23:29:10 +0530

    📜

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; // eslint-disable-next-line
 import DetailsContextProvider from './contexts/DetailsContext'
 
 
