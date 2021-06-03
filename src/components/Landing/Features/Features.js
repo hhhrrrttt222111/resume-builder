37a24b277cfd94332cd29eafe19e6c4b801158dc
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2021-06-03 20:17:33 +0530

    📜

--- src/components/Landing/Features/Features.js
+++ src/components/Landing/Features/Features.js
@@ -5,7 +5,22 @@ import './Features.css'
 function Features() {
     return (
         <div className="features">
-            <h1>Features</h1>
+            <div className="features-body">
+                <div className="features-description">
+                    <div className="features-text">
+                        <h2>Beautiful Designs</h2>
+                        <p>Customize your design to suit your own preferences to create an online portfolio which is truly unique. Preview changes instantly without affecting your live site and without writing any code.</p>
+                    </div>
+                    <div className="features-text">
+                        <h2>Beautiful Designs</h2>
+                        <p>Customize your design to suit your own preferences to create an online portfolio which is truly unique. Preview changes instantly without affecting your live site and without writing any code.</p>
+                    </div>
+                </div>
+                <div className="features-img">
+                    <img src = "https://images.unsplash.com/photo-1622557875141-e431587c0d28?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3MXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt=""/>
+                </div>
+
+            </div>
         </div>
     )
 }
